4a7584bd1b48bddf8f4fbf7ec5aef5c144b80651
7up <23521320@gm.uit.edu.vn>
2025-10-31 09:18:10 +0700

    -

--- levels/map_1.gd
+++ levels/map_1.gd
@@ -1,9 +1,9 @@
-#extends Node
-#func _enter_tree() -> void:
-	#GameManager.current_stage = self
-#
-#func _ready() -> void:
-	## ⏳ Delay 1 frame cho toàn bộ Portal, TileMap load xong
-	#await get_tree().process_frame
-	#if not GameManager.respawn_at_portal():
-		#GameManager.respawn_at_checkpoint()
+extends Node
+
+func _enter_tree() -> void:
+	# Handle portal spawning first
+	GameManager.current_stage = self
+	
+func _ready() -> void:
+	if not GameManager.respawn_at_portal():
+		GameManager.respawn_at_checkpoint()
