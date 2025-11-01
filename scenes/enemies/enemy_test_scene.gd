3197b4c6248e14a80eb1a8ccf68a5cf04cb83277
MKwang <tmq1512005@gmail.com>
2025-11-01 23:48:37 +0700

    feat(player): Added preventing players from going through spikes while invulnerable

--- scenes/enemies/enemy_test_scene.gd
+++ scenes/enemies/enemy_test_scene.gd
@@ -5,11 +5,5 @@ func _enter_tree() -> void:
 	GameManager.current_stage = self
 	
 func _ready() -> void:
-	$Player.spike_collision.connect(_set_spike_collision)
 	if not GameManager.respawn_at_portal():
 		GameManager.respawn_at_checkpoint()
-
-func _set_spike_collision(enable :bool):
-	var spikes = find_children("Spike", "Node", true, false)
-	for s in spikes:
-		s.set_collision(enable)
