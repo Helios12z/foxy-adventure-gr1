82d368fb2491182f23e52c30605dbed933e0d8fc
caoquang284 <rinquanq05@gmail.com>
2025-11-03 12:52:08 +0700

    feat(npc): add crystal mauler to give foxy the blade

--- scenes/items/blade.gd
+++ scenes/items/blade.gd
@@ -1,11 +1,11 @@
 extends StaticBody2D
 
 func _ready() -> void:
-    if has_node("PickupArea"):
-        var area: Area2D = $PickupArea
-        area.body_entered.connect(_on_pickup_body_entered)
+	if has_node("PickupArea"):
+		var area: Area2D = $PickupArea
+		area.body_entered.connect(_on_pickup_body_entered)
 
 func _on_pickup_body_entered(body: Node) -> void:
-    if body is Player:
-        body.collected_blade()
-        queue_free()
+	if body is Player:
+		body.collected_blade()
+		queue_free()
