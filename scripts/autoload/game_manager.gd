9aa08fedcc74b82995f5d84b9556463f5d6eb989
MKwang <tmq1512005@gmail.com>
2025-11-25 16:22:07 +0700

    Refactor collectibles and add powerup system
    Moved coin, blade, chest, key, and gem scenes/scripts from items/objects to collectibles/. Introduced a new inventory system and powerup system, including blade and magnet powerups and their database. Updated references in tilesets, levels, and test scenes to use new collectible paths. Removed obsolete item scripts and scenes.

--- scripts/autoload/game_manager.gd
+++ scripts/autoload/game_manager.gd
@@ -9,10 +9,14 @@ var checkpoint_data: Dictionary = {}
 var current_stage: Node = null
 var player: Player = null
 
+var inventory_system: InvetorySystem = null
+
 func _ready() -> void:
 	# Load checkpoint data when game starts
 	load_checkpoint_data()
-	
+		# Init inventory system
+	inventory_system = InvetorySystem.new()
+	add_child(inventory_system)
 	pass
 
 #change stage by path and target portal name
---
+++ scripts/inventory_system.gd
@@ -0,0 +1,41 @@
+extends Node
+class_name InvetorySystem
+
+signal coin_changed(new_amount: int)
+signal item_collected(item_type: String, amount: int)
+
+var coins: int = 0
+var keys: int = 0
+
+func _ready() -> void:
+	pass
+	
+func add_coin(amount: int) -> void:
+	coins += amount
+	coin_changed.emit(coins)
+	item_collected.emit("coin", amount)
+	print("Collected ", amount, " coins. Total: ", coins)
+	
+func add_key(_amount: int = 1) -> void:
+	keys += _amount
+	item_collected.emit("key", _amount)
+	print("Collected ", _amount, " key Total: ", keys)
+	
+func use_key() -> bool:
+	if has_key():
+		keys -= 1
+		return true
+	return false
+	
+	
+
+func has_key() -> bool:
+	return keys > 0	
+
+func get_gold() -> int:
+	return coins
+
+func get_keys() -> int:
+	return keys
+
+	
