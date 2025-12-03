dad73f05551b1af67b4b4efdec89c5cdb479b791
Hien <nguyenhoanghien.2505@gmail.com>
2025-12-03 07:09:22 +0700

    feat(enemies): add cray fish and golden carp enemy behaviors
    Implement enemy behaviors including walk, chase, and attack states for both cray fish and golden carp enemies. Add test map with enemy placements and interactions.

---
+++ scenes/enemies/golden_carp/cray_fish.gd
@@ -0,0 +1,47 @@
+extends EnemyCharacter
+
+@export var minion_health: int = 100
+@export var bob_amplitude: float = 3.0
+@export var bob_speed: float = 2.0
+
+@onready var detect_front = $Direction/DetectFrontRayCast2D
+@onready var detect_back = $Direction/DetectBackRayCast2D
+@onready var attack_scope_ray_cast = $Direction/AttackScopeRayCast2D
+@onready var hurt_area = $Direction/HurtArea2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $Direction/AnimatedSprite2D
+
+var _bob_t: float = 0.0
+var _base_sprite_y: float = 0.0
+
+func _ready() -> void:
+	max_health = minion_health
+	super._ready()
+	fsm = FSM.new(self, $States, $States/Walk)
+	set_hit_collision(false)
+	hurt_area.hurt.connect(_on_hurt_area_2d_hurt)
+	detect_front.enabled = true
+	detect_back.enabled = true
+	attack_scope_ray_cast.enabled = true
+	_base_sprite_y = animated_sprite_2d.position.y
+
+func _process(delta: float) -> void:
+	_bob_t += delta
+	animated_sprite_2d.position.y = _base_sprite_y + sin(_bob_t * bob_speed) * bob_amplitude
+
+func can_detect_player() -> bool:
+	if detect_front.is_colliding():
+		var obj = detect_front.get_collider()
+		return true
+
+	if detect_back.is_colliding():
+		var obj = detect_back.get_collider()
+		turn_around()
+		_check_changed_direction()
+		return true
+
+	return false
+
+func is_in_attack_scope() -> bool:
+	if attack_scope_ray_cast.is_colliding():
+		return true
+	return false
