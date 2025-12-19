8f859327f0b78fd55e2fbb5ddda0b5b5229f89ad
nguyenminhquang <nnguyenminhquang786@gmail.com>
2025-12-20 00:45:08 +0700

    feat: update skeleton revival

--- scenes/enemies/skeleton/resurrect.gd
+++ scenes/enemies/skeleton/resurrect.gd
@@ -3,13 +3,10 @@ extends EnemyState
 var sprite: AnimatedSprite2D
 
 func _enter() -> void:
+	obj.health = obj.max_health
 	obj.change_animation("resurrect")
 	sprite = obj.animated_sprite_2d
 	await sprite.animation_finished
-	obj.is_dead = false
-	obj.finished_in_temporary_dead = false
-	obj.is_in_temporary_dead = false
-	obj.health = obj.max_health
+	obj.velocity.x = 0
 	obj._update_health_bar_after_damage()
-	obj.reset_healthbar_visual()
 	fsm.change_state(fsm.states.idle)
