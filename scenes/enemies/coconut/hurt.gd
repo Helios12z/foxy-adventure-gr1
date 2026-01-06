2ba0ab7c8c29bac40ac346381e49a1225f94e3f1
7up <23521320@gm.uit.edu.vn>
2026-01-06 17:56:23 +0700

    feat: added coconut hurt effect, added coconut to map 1, spider and lights to map 2

--- scenes/enemies/coconut/hurt.gd
+++ scenes/enemies/coconut/hurt.gd
@@ -1,21 +1,75 @@
 extends EnemyState
 
-var _damage_direction: Vector2
-
-func take_damage(damage_dir: Vector2, _damage: int) -> void:
-	_damage_direction = damage_dir
-	change_state(fsm.states.hurt)
+var _skew_tween: Tween
+var _direction_node: Node2D
 
 func _enter() -> void:
 	obj.change_animation("hurt")
-	obj.velocity = Vector2.ZERO
 	timer = 0.4
+
+	# Get Direction node for visual effects
+	if obj.has_node("Direction"):
+		_direction_node = obj.get_node("Direction")
+
+	# Apply knockback with smooth knock-up
+	_apply_knockback()
+
+	# Apply skew effect for impact feel
+	_apply_skew_effect()
 
 func _update(delta: float) -> void:
 	if update_timer(delta):
+		_cleanup_effects()
+
 		if obj.health <= 0:
 			change_state(fsm.states.dead)
 		elif obj.can_detect_player():
 			change_state(fsm.states.attack)
 		else:
 			change_state(fsm.states.walk)
+
+func _apply_knockback():
+	# Horizontal knockback is already applied in EnemyState.take_damage()
+	# Here we add vertical knock-up for better feel
+
+	# Only apply upward knockback for enemies with gravity (ground enemies)
+	# Flying enemies (gravity = 0) won't fly away when hit
+	if obj.gravity > 0:
+		# Smooth knock-up with decay
+		obj.velocity.y = -150
+	else:
+		# For flying enemies, add slight recoil backwards
+		# The horizontal velocity is already set, just enhance it slightly
+		obj.velocity.x *= 1.2
+
+func _apply_skew_effect():
+	if _direction_node == null:
+		return
+
+	# Kill existing tween if any
+	if _skew_tween != null and _skew_tween.is_valid():
+		_skew_tween.kill()
+
+	# Create skew animation for impact
+	var skew_direction = sign(obj.velocity.x)
+	if skew_direction == 0:
+		skew_direction = -obj.direction
+
+	_skew_tween = create_tween()
+	_skew_tween.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+
+	# Skew on impact
+	_skew_tween.tween_property(_direction_node, "skew", skew_direction * 0.3, 0.1)
+	# Return to normal
+	_skew_tween.tween_property(_direction_node, "skew", 0.0, 0.3)
+
+func _cleanup_effects():
+	# Clean up skew effect
+	if _direction_node != null:
+		_direction_node.skew = 0.0
+
+	if _skew_tween != null and _skew_tween.is_valid():
+		_skew_tween.kill()
+
+func _exit():
+	_cleanup_effects()
